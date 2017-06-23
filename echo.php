9b0f3ad13357dd9c3668b9971884373a7b09875b
EnTi <entwoti@gmail.com>
2017-06-23 17:07:43 +0700

    Echo msg

--- echo.php
+++ echo.php
@@ -3,4 +3,7 @@
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 	use juno_okyo\Chatfuel;
 
-	
+	if (isset($_GET['msg') && !empty($_GET['msg'])) {
+		$cf = new Chatfuel();
+		$cf->sendText($_GET['msg']);
+	}
